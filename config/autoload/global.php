56237f9459263e67ce7816fa2b1295c7a476e7bb
dacolera <dacolera360@gmail.com>
2015-08-24 22:09:47 -0300

    instala tema administrativo

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -12,5 +12,20 @@
  */
 
 return array(
-    // ...
+    'db' => array(
+        'driver' => 'Pdo',
+        'dsn' => 'mysql:dbname=ierp;host=localhost',
+        'username' => 'root',
+        'password' => '2013',
+        'driver_options' => array(
+            PDO::MYSQL_ATTR_INIT_COMMAND =>
+                'SET NAMES \'UTF8\''
+        ),
+    ),
+    'service_manager' => array(
+        'factories' => array(
+            'Zend/Db/Adapter'
+            => 'Zend\Db\Adapter\AdapterServiceFactory'
+        )
+    )
 );
