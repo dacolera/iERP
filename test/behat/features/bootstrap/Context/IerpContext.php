a66b04d1d887bfbd7421a131bbe629d401008729
jurandir dacol <dacolera360@gmail.com>
2015-09-11 13:46:35 -0300

    behat e phpunit iniciado

---
+++ test/behat/features/bootstrap/Context/IerpContext.php
@@ -0,0 +1,11 @@
+<?php
+namespace Context;
+
+use Behat\MinkExtension\Context\MinkContext;
+use Behat\Behat\Context\Context;
+use Behat\Testwork\Tester\Result\TestResult;
+
+final class IerpContext implements Context
+{
+	
+}
---
+++ test/behat/features/exemplo.feature
@@ -0,0 +1,9 @@
+Feature:
+	funcionalidade de exemplo
+
+	Scenario: testando a home
+		Given I am on the homepage
+		Then print last response
+		And I should see a "#login" element
+
+
